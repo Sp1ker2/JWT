75c0a187be55686dab15c41edfe23087c3415b18
Invalidsyntaxisbot <101515672+Sp1ker2@users.noreply.github.com>
2025-01-17 11:46:35 +0200

    Add files via upload

---
+++ core/config.py
@@ -0,0 +1,28 @@
+from pathlib import Path
+from pydantic import BaseModel
+from pydantic_settings import BaseSettings
+
+BASE_DIR = Path(__file__).parent.parent
+
+DB_PATH = BASE_DIR / "db.sqlite3"
+
+
+class DbSettings(BaseModel):
+    url: str = f"sqlite+aiosqlite:///{DB_PATH}"
+    # echo: bool = False
+    echo: bool = True
+
+
+class AuthJWT(BaseModel):
+    private_key_path:Path = BASE_DIR/"certs"/"jwt-private.pem"
+    public_key_path:Path = BASE_DIR/"certs"/"jwt-public.pem"
+    algorithm :str="R256"
+class Settings(BaseSettings):
+    api_v1_prefix: str = "/api/v1"
+
+    db: DbSettings = DbSettings()
+    auth_jwt:AuthJWT = AuthJWT( )
+    # db_echo: bool = True
+
+
+settings = Settings()
